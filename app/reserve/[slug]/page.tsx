0d38336a4a8fd07dfe47b94811d28210d2633b9d
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-07-24 20:22:43 -0300

    Use layout and remove repetition

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,22 +1,14 @@
-import Navbar from "@/app/components/Navbar"
-import Link from "next/link"
 import Header from "../components/Header"
 import Form from "../components/Form"
 
 function ReservationPage() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <Navbar />
-
-        <div className="border-t h-screen">
-          <div className="py-9 w-3/5 m-auto">
-            <Header />
-            <Form />
-          </div>
-        </div>
-      </main>
-    </main>
+    <div className="border-t h-screen">
+      <div className="py-9 w-3/5 m-auto">
+        <Header />
+        <Form />
+      </div>
+    </div>
   )
 }
 
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,5 +1,3 @@
-import Navbar from "@/app/components/Navbar"
-import React from "react"
 import Header from "./components/Header"
 import RestaurantNavbar from "./components/RestaurantNavbar"
 import Title from "./components/Title"
@@ -11,27 +9,24 @@ import ReservartionCard from "./components/ReservartionCard"
 
 function RestaurantDetails() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <Navbar />
-        <Header />
+    <>
+      <Header />
 
-        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-          <div className="bg-white w-[70%] rounded p-3 shadow">
-            <RestaurantNavbar />
-            <Title />
-            <Rating />
-            <Description />
-            <Images />
-            <Reviews />
-          </div>
-
-          <div className="w-[27%] relative text-reg">
-            <ReservartionCard />
-          </div>
+      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
+        <div className="bg-white w-[70%] rounded p-3 shadow">
+          <RestaurantNavbar />
+          <Title />
+          <Rating />
+          <Description />
+          <Images />
+          <Reviews />
         </div>
-      </main>
-    </main>
+
+        <div className="w-[27%] relative text-reg">
+          <ReservartionCard />
+        </div>
+      </div>
+    </>
   )
 }
 
