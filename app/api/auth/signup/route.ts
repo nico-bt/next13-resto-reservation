fd03f87900a1283493fef221239d6365506b62ac
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-08-20 23:51:34 -0300

    Add signup route. Validate user input with validator package

---
+++ app/api/auth/signup/route.ts
@@ -0,0 +1,53 @@
+import { NextRequest, NextResponse } from "next/server"
+import validator from "validator"
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  const { firstName, lastName, email, phone, city, password } = await req.json()
+
+  const errors: string[] = []
+
+  const validationSchema = [
+    {
+      valid: validator.isLength(firstName, {
+        min: 1,
+        max: 30,
+      }),
+      errorMessage: "First name is invalid",
+    },
+    {
+      valid: validator.isLength(lastName, {
+        min: 1,
+        max: 30,
+      }),
+      errorMessage: "Last name is invalid",
+    },
+    {
+      valid: validator.isEmail(email),
+      errorMessage: "Email is invalid",
+    },
+    {
+      valid: validator.isMobilePhone(phone),
+      errorMessage: "Phone number is invalid",
+    },
+    {
+      valid: validator.isLength(city, { min: 1 }),
+      errorMessage: "City is invalid",
+    },
+    {
+      valid: validator.isLength(password, { min: 1 }),
+      errorMessage: "Password required",
+    },
+  ]
+
+  validationSchema.forEach((check) => {
+    if (!check.valid) {
+      errors.push(check.errorMessage)
+    }
+  })
+
+  if (errors.length) {
+    return NextResponse.json({ errorMessage: errors[0] }, { status: 400 })
+  }
+
+  return NextResponse.json({ email })
+}
